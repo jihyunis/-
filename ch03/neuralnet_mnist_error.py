e69f1560a2fa8fb18045df39d7b8ddc8fcb7f5f7
jihyunis <99638527+jihyunis@users.noreply.github.com>
2022-02-15 20:02:32 +0900

    Add files via upload

---
+++ ch03/neuralnet_mnist_error.py
@@ -0,0 +1,56 @@
+# coding: utf-8
+import sys, os
+sys.path.append(os.pardir)  # 부모 디렉터리의 파일을 가져올 수 있도록
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+from dataset.mnist import load_mnist
+from common.functions import sigmoid, softmax
+
+
+def get_data():
+    (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, flatten=True, one_hot_label=False)
+    return x_test, t_test
+
+
+def init_network():
+    with open("sample_weight.pkl", 'rb') as f:
+        network = pickle.load(f)
+    return network
+
+
+def predict(network, x):
+    w1, w2, w3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+
+    a1 = np.dot(x, w1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, w2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, w3) + b3
+    y = softmax(a3)
+
+    return y
+
+
+x, t = get_data()
+network = init_network()
+error = []
+P=[]
+for i in range(len(x)):
+    y = predict(network, x[i])
+    p = np.argmax(y) # 확률이 가장 높은 원소의 인덱스를 얻는다.
+    if p != t[i]:
+        error.append(i)
+        P.append(p)
+
+print(error)
+
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.imshow(x[error[i]].reshape(28,28), cmap=plt.cm.binary)
+    plt.xlabel(str(t[error[i]])+"==>"+str(P[i]))
+plt.show()
